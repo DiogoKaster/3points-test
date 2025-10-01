7cd06eec58e65ab0cf868f5d9ebe0f2ab438c81f
kaster <diogokaster@gmail.com>
2025-10-01 07:05:38 -0300

    feat(votes): add post votes functionality

--- app/Models/Traits/HasVotes.php
+++ app/Models/Traits/HasVotes.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Models\Traits;
 
+use App\Models\User;
 use App\Models\Vote;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
 
@@ -15,5 +16,38 @@ trait HasVotes
     public function votes(): MorphMany
     {
         return $this->morphMany(Vote::class, 'votable');
+    }
+
+    public function vote(User $user, int $type): void
+    {
+        $type = in_array($type, [1, -1], true) ? $type : 0;
+        if ($type === 0) {
+            return;
+        }
+
+        $existingVote = $this->votes()->where('user_id', $user->id)->first();
+
+        if ($existingVote) {
+            if ($existingVote->type === $type) {
+                $this->votes += -$existingVote->type;
+                $this->save();
+
+                $existingVote->delete();
+            } else {
+                $delta = $type - $existingVote->type;
+                $this->votes += $delta;
+                $this->save();
+
+                $existingVote->update(['type' => $type]);
+            }
+        } else {
+            $this->votes += $type;
+            $this->save();
+
+            $this->votes()->create([
+                'user_id' => $user->id,
+                'type' => $type,
+            ]);
+        }
     }
 }
